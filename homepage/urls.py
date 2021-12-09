37e67462bddb3ee54e6eb898e6279b7149cb3751
Bimal Shrestha <bimalstha291@gmail.com>
2021-12-09 21:34:35 +0545

    Login page first phase design

--- homepage/urls.py
+++ homepage/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index_page, name='index_page'),
+    path('login', views.login_page, name='login_page')
 ]
--- homepage/views.py
+++ homepage/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, 'homepage/homepage.html')
+
+
+def login_page(request):
+    return render(request,'homepage/login.html')
