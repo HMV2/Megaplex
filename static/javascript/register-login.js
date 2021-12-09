72e96be1f9befaf590725b7205b56fa2561844d8
Nis-chal <nischalchan@gmail.com>
2021-12-09 20:46:55 +0545

    register was initial design was created

---
+++ static/javascript/register-login.js
@@ -0,0 +1,30 @@
+let showPassword = document.querySelectorAll('.eyeIcon')
+let password = document.querySelectorAll('.password')
+
+function toggleHidden(){
+    let inputElement = this.parentElement.children[1]
+    
+
+
+
+
+    if(inputElement.type === "password"){
+        inputElement.type = "text"
+        this.classList.toggle('fa-eye-slash')
+        this.classList.toggle('fa-eye')
+        
+     
+    }
+    else{
+        inputElement.type = "password"
+        this.classList.toggle('fa-eye-slash')
+        this.classList.toggle('fa-eye')
+        
+    }
+}
+
+
+showPassword.forEach((item) => {
+
+    item.addEventListener('click', toggleHidden)
+});
