f76f10948afda46f76774c882fcfb9e38952bde9
Bimal Shrestha <bimalstha291@gmail.com>
2021-12-10 12:28:34 +0545

    Registration page second phase design

--- static/javascript/register-login.js
+++ static/javascript/register-login.js
@@ -3,23 +3,16 @@ let password = document.querySelectorAll('.password')
 
 function toggleHidden(){
     let inputElement = this.parentElement.children[1]
-    
-
-
-
 
     if(inputElement.type === "password"){
         inputElement.type = "text"
         this.classList.toggle('fa-eye-slash')
         this.classList.toggle('fa-eye')
-        
-     
     }
     else{
         inputElement.type = "password"
         this.classList.toggle('fa-eye-slash')
         this.classList.toggle('fa-eye')
-        
     }
 }
 
