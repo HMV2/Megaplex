997ffd830819c5048e6b2d1eddfc525b54538e87
Bimal Shrestha <bimalstha291@gmail.com>
2021-12-27 19:56:29 +0545

    Seperate navbar for authenticated user and sweet alert fixed

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import logout
 
 
 def index_page(request):
     return render(request, 'homepage/homepage.html')
 
+def logout_view(request):
+    logout(request)
+    return redirect('/')
