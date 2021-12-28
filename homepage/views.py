d910b88b66f8edad7c43afbfe82fb4916bc09ed4
Bimal Shrestha <bimalstha291@gmail.com>
2021-12-28 17:58:58 +0545

    Category second phase

--- homepage/views.py
+++ homepage/views.py
@@ -1,13 +1,30 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import logout
 
+from admins.models import Category
+
 
 def index_page(request):
     return render(request, 'homepage/homepage.html')
+
 
 def logout_view(request):
     logout(request)
     return redirect('/')
 
+
 def category(request):
-    return render(request, 'homepage/category.html')
+    data = Category.objects.all().order_by('-id')
+    context = {
+        'data': data
+    }
+    return render(request, 'homepage/category.html', context)
+
+# data = Country.objects.all().order_by('-id')
+#     # country_filter = CountryFilter(request.GET, queryset=data)
+#     context = {
+#         'data': data,
+#         'activate_explore': 'active border-bottom active-class',
+#         # 'country_filter': country_filter
+#     }
+#     return render(request, 'dashboard/country.html', context)
