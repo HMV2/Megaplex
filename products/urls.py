4e9b6b5559cfba859da6d7d6803a29cd0eca4797
Chirag <chiragsimkhada@gmail.com>
2021-12-31 13:29:33 +0545

    Removed faulty app

--- products/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import views as auth_views
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('details/<int:product_id>', views.product_details, name='Details_page')
-]
