d3c5f5822edb43fd69f3d44ec640adb31f4e81ab
Chirag <chiragsimkhada@gmail.com>
2021-12-31 14:10:48 +0545

    Completed the chat section

--- product/views.py
+++ product/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth.models import User
+from directChat.models import Chat_Message
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.http.response import HttpResponseBadRequest
+from django.contrib import messages
 
 def product_details(request,product_id):
     product = Product.objects.get(id=product_id)
     context={
         'product':product
     }
+    if request.method == 'POST' and request.POST.get('body')!="":
+        from_user = request.user
+        to_user_username = request.POST.get('to_user')
+        body = request.POST.get('body')
+        print("body "+body)
+        try:
+            to_user = User.objects.get(username=to_user_username)
+        except:
+            to_user = User.objects.get(id=request.POST.get('to_user'))
+        Chat_Message.send_message(from_user, to_user, body)
+        messages.success(request,"Message Sent Successfully!")
+        return render(request, 'product/details.html',context)
+    elif request.method == 'POST' and request.POST.get('body')=="":
+        messages.error(request,"Please insert message to send!")
+        return render(request, 'product/details.html',context)
+        # HttpResponseBadRequest()
+
+
     return render(request, 'product/details.html',context)
+
