607c8019cc485665ee7f5cd2db77d0903530353e
Chirag <chiragsimkhada@gmail.com>
2021-12-31 14:44:17 +0545

    Added backend to profile page 70% completed

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,26 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from account.models import Profile
+from product.models import Product
 # Create your views here.
+
+@login_required
 def profile(request):
-    return render(request, 'dashboard/profile.html')
+    user = Profile.objects.get(user=request.user)
+    try:
+        active_products = Product.objects.filter(seller=request.user, is_active=True)
+    except:
+        active_products = None
+    try:
+        inactive_products = Product.objects.filter(seller=request.user, is_active=False)
+    except:
+        inactive_products = None
+
+    print(active_products)
+    print(inactive_products)
+    context={
+        'profile':user,
+        'active_products':active_products,
+        'inactive_products':inactive_products
+    }
+    return render(request, 'dashboard/profile.html',context)
