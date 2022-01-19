38af8fd9fb1bab3f0a1b39b54d96ad35b2f57be7
Chirag <chiragsimkhada@gmail.com>
2022-01-19 20:58:46 +0545

    Added Password reset feature

--- account/urls.py
+++ account/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     path('login/', views.login, name="login_user"),
     path('logout/', views.logout_user),
 
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name="account/password_reset_form.html"), name="password_reset"),
+    path('password_reset_sent/', auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset_sent.html"), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset_confirm.html"), name="password_reset_confirm"),
+    path('reset/done', auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset_complete.html"), name="password_reset_complete"),
+    
+
 ]
