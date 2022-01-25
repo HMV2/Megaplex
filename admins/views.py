08bb7d8b51f7b274f05a07fbce5008d2160d2d2c
Bimal Shrestha <bimalstha291@gmail.com>
2022-01-25 11:44:50 +0545

    Some changes

--- admins/views.py
+++ admins/views.py
@@ -1,6 +1,19 @@
+import profile
 from django.shortcuts import render
-
+from django.template import context
+from django.contrib.auth.models import User
+from account.models import Profile
 
 # Create your views here.
 def dashboard(request):
-    return render(request, 'admins/dashboard.html')
+    users = User.objects.all()
+    profile = Profile.objects.all()
+    user_info = users.filter(is_staff=0)
+    admin_info = users.filter(is_staff=1)
+    context={
+        'admin_info':admin_info,
+        'user_info':user_info,
+        'profile': profile
+        
+    }
+    return render(request, 'admins/dashboard.html',context)
