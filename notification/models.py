b8fecf15769b668b4778a8f0279ccec0e9558d50
Bimal Shrestha <bimalstha291@gmail.com>
2022-02-19 12:25:17 +0545

    Category card modifies

--- notification/models.py
+++ notification/models.py
@@ -1,11 +1,15 @@
 import json
+from tokenize import String
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django_celery_beat.models import PeriodicTask, CrontabSchedule
+from pyparsing import Char
 # Create your models here.
 
+
 class BroadcastNotification(models.Model):
+    title = models.CharField(default="Megaplex Notification", max_length=120)
     message = models.TextField()
     broadcast_on = models.DateTimeField()
     sent = models.BooleanField(default=False)
